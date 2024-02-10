88d91d20ea65427b088c894d473ba1ee15d7ad5a
Shravan Asati <dev.shravan@protonmail.com>
2024-02-10 20:44:53 +0530

    move everything in one file + input validation

--- src/crusade.cpp
+++ src/crusade.cpp
@@ -1,7 +1,261 @@
+#include <bits/stdc++.h>
 #include <iostream>
-#include "stringutils.hpp"
-#include "lexer.hpp"
-#include "ranker.hpp"
+
+/* * STRINGUTILS * */
+// trim from start (in place)
+inline void ltrim(std::string& s) {
+    s.erase(s.begin(), std::find_if(s.begin(), s.end(), [](unsigned char ch) {
+                return !std::isspace(ch);
+            }));
+}
+
+// trim from end (in place)
+inline void rtrim(std::string& s) {
+    s.erase(std::find_if(s.rbegin(), s.rend(),
+                         [](unsigned char ch) { return !std::isspace(ch); })
+                .base(),
+            s.end());
+}
+
+// trim string from both ends
+inline void trim(std::string& s) {
+    ltrim(s);
+    rtrim(s);
+}
+
+/* * TOKENS * */
+enum class Associativity { LEFT, RIGHT };
+
+enum class TokenType {
+    NUMBER,
+    PLUS_OP,
+    MINUS_OP,
+    MULTIPLY_OP,
+    DIVIDE_OP,
+    FLOOR_DIVIDE_OP,
+    EXPONENT_OP,
+    LPAREN,
+    RPAREN
+};
+
+class Token {
+protected:
+    std::string value;
+    TokenType tokenType;
+    int precedence;
+    Associativity associativity;
+
+public:
+    Token(const std::string& value, TokenType tokenType, int precedence,
+          Associativity associativity)
+        : value(value), tokenType(tokenType), precedence(precedence),
+          associativity(associativity) {}
+
+    std::string getValue() const { return value; }
+    TokenType getTokenType() const { return tokenType; }
+    int getPrecedence() const { return precedence; }
+    Associativity getAssociativity() const { return associativity; }
+
+    bool operator==(const Token& other) const {
+        return tokenType == other.tokenType;
+    }
+};
+
+Token NumberToken(std::string value) {
+    return Token(value, TokenType::NUMBER, 0, Associativity::LEFT);
+}
+
+Token PlusToken() {
+    return Token("+", TokenType::PLUS_OP, 1, Associativity::LEFT);
+}
+
+Token MinusToken() {
+    return Token("-", TokenType::MINUS_OP, 1, Associativity::LEFT);
+}
+
+Token MultiplyToken() {
+    return Token("*", TokenType::MULTIPLY_OP, 2, Associativity::LEFT);
+}
+
+Token DivideToken() {
+    return Token("/", TokenType::DIVIDE_OP, 2, Associativity::LEFT);
+}
+
+Token FloorDivideToken() {
+    return Token("//", TokenType::FLOOR_DIVIDE_OP, 2, Associativity::LEFT);
+}
+
+Token ExponentToken() {
+    return Token("^", TokenType::EXPONENT_OP, 3, Associativity::RIGHT);
+}
+
+Token LPARENToken() {
+    return Token("(", TokenType::LPAREN, 0, Associativity::LEFT);
+}
+
+Token RPARENToken() {
+    return Token(")", TokenType::RPAREN, 0, Associativity::LEFT);
+}
+
+/* * LEXER * */
+
+class Lexer {
+private:
+    const std::string& infix;
+    int position;
+    char currentChar;
+    int rank;
+    std::stack<int> bracketPositions;
+
+    void advance() {
+        position++;
+        currentChar = infix.at(position);
+    }
+
+    bool isDigit(char c) { return c >= '0' && c <= '9'; }
+
+    bool isDecimal(char c) { return c == '.'; }
+
+    Token makeNumberToken() {
+        // todo keep count of decimals
+        std::string number;
+        while (true) {
+            if (isDigit(currentChar) || isDecimal(currentChar)) {
+                number += currentChar;
+                try {
+                    advance();
+                } catch (std::out_of_range) {
+                    return NumberToken(number);
+                }
+            } else {
+                // since current char is not a digit or a decimal
+                // it maybe a operator which will be advanced upon
+                // inside the loop, so decrease the position by 1
+                position--;
+                break;
+            }
+        }
+        return NumberToken(number);
+    }
+
+    void reportError(std::string error, int offset = -1) {
+        if (offset < 0) {
+            offset = position;
+        }
+        std::cout << infix << "\n";
+        std::cout << std::string(offset, ' ');
+        std::cout << "^ " << error << std::endl;
+    }
+
+public:
+    Lexer(const std::string& input) : infix(input) {
+        position = -1;
+        rank = 0;
+    }
+
+    std::vector<Token> tokenize() {
+        std::vector<Token> tokens;
+        advance(); // not checking for exception here because empty input is
+                   // ignored by the input loop
+        bool reachedEnd = false;
+        while (!reachedEnd) {
+            if (std::isspace(currentChar)) {
+                goto next;
+            }
+
+            if (currentChar == '+') {
+                tokens.push_back(PlusToken());
+            } else if (currentChar == '-') {
+                tokens.push_back(MinusToken());
+            } else if (currentChar == '*') {
+                tokens.push_back(MultiplyToken());
+            } else if (currentChar == '/') {
+                if (position == (infix.length() - 1)) {
+                    // no need to check for floor division if the end of input
+                    // is reached
+                    tokens.push_back(DivideToken());
+                } else {
+                    // try advancing to check for floor division operator
+                    advance();
+                    if (currentChar == '/') {
+                        tokens.push_back(FloorDivideToken());
+                    } else {
+                        tokens.push_back(DivideToken());
+                        // go back one character since we advanced once more to
+                        // check next character
+                        position--;
+                    }
+                }
+            } else if (currentChar == '^') {
+                tokens.push_back(ExponentToken());
+            } else if (currentChar == '(') {
+                tokens.push_back(LPARENToken());
+                bracketPositions.push(position);
+            } else if (currentChar == ')') {
+                if (bracketPositions.empty()) {
+                    // no open bracket is set for this to close
+                    reportError("missing opening parentheses");
+                    return std::vector<Token>{};
+                }
+                tokens.push_back(RPARENToken());
+                bracketPositions.pop();
+            } else if (isDigit(currentChar) || isDecimal(currentChar)) {
+                auto numberToken = makeNumberToken();
+                tokens.push_back(numberToken);
+            } else {
+                reportError("invalid character");
+                return std::vector<Token>{};
+            }
+
+            {
+                // * new scope to allow control transfer from goto
+                auto lastTokenType = tokens.back().getTokenType();
+                auto isNumber = lastTokenType == TokenType::NUMBER;
+                auto isParen = lastTokenType == TokenType::LPAREN ||
+                               lastTokenType == TokenType::RPAREN;
+                if (isNumber) {
+                    rank++;
+                } else if (!isParen) {
+                    rank--;
+                }
+
+                if (rank < 0) {
+                    reportError("missing operand");
+                    return std::vector<Token>{};
+                } else if (rank > 1) {
+                    reportError("missing operator");
+                    return std::vector<Token>{};
+                }
+            }
+
+        next: {
+            try {
+                advance();
+            } catch (std::out_of_range) {
+                reachedEnd = true;
+            }
+        }
+        }
+
+		// rank checking is different here because the final value of an expression must be
+		// equal to one. the range of rank is [0, 1] inside loop. 
+        if (rank < 1) {
+            reportError("missing operand");
+            return std::vector<Token>{};
+        } else if (rank > 1) {
+            reportError("missing operator");
+            return std::vector<Token>{};
+        }
+
+        if (!bracketPositions.empty()) {
+            reportError("missing closing parentheses", bracketPositions.top());
+            return std::vector<Token>{};
+        }
+        return tokens;
+    }
+};
+
+/* * CRUSADE * */
 
 int main(int argc, char const* argv[]) {
     while (true) {
@@ -10,29 +264,24 @@ int main(int argc, char const* argv[]) {
         std::getline(std::cin, input);
         trim(input);
 
-		if (input == "") {
-			continue;
-		}
+        if (input == "") {
+            continue;
+        }
 
         if (input == "quit" || input == "exit" || input == "/q") {
             std::cout << "bye" << std::endl;
             break;
         }
-        
-		auto lexer = Lexer(input);
-		auto tokens = lexer.tokenize();
-		if (tokens.size() == 0) {
-			// lexer encountered an error during tokenization
-			continue;
-		}
 
-		auto ranker = Ranker(tokens);
-		auto rankResult = ranker.evaluate();
-		if (!rankResult.success) {
-			std::cout << rankResult.message << "\n";
-			continue;
-		}
+        auto lexer = Lexer(input);
+        auto tokens = lexer.tokenize();
+        if (tokens.size() == 0) {
+            // lexer encountered an error during tokenization, the expression is invalid
+            continue;
+        }
 
+        std::cout << "success"
+                  << "\n";
     }
 
     return 0;
--- src/stringutils.hpp
+++
@@ -1,5 +0,0 @@
-#include <string>
-
-void trim(std::string& s);
-void ltrim(std::string& s);
-void rtrim(std::string& s);
